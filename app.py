88f985013871aab33f2b6b40ce2fb78eae8cfa0e
malith123W <malithinduranga2001@gmail.com>
2025-05-11 00:03:52 +0530

    logger

--- app.py
+++ app.py
@@ -1,40 +1,49 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request, redirect
 from helper import preprocessing, vectorizer, get_prediction
+from logger import logging
 
 app = Flask(__name__)
 
-data =dict()
-reveiws =[]
-positive=0
-negative=0
+logging.info('Flask server started')
 
+data = dict()
+reviews = []
+positive = 0
+negative = 0
 
 @app.route("/")
 def index():
-    data['reviews'] = reveiws
+    data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info('========== Open home page ============')
+
     return render_template('index.html', data=data)
 
-
-@app.route("/", methods ={'post'})
+@app.route("/", methods = ['post'])
 def my_post():
     text = request.form['text']
-    preproced_text = preprocessing(text)
-    vectorized_text = vectorizer(preproced_text)
-    prediction = get_prediction(vectorized_text)
+    logging.info(f'Text : {text}')
 
-    if prediction == 'positive':
-        global positive
-        positive+=1
-    else:
+    preprocessed_txt = preprocessing(text)
+    logging.info(f'Preprocessed Text : {preprocessed_txt}')
+
+    vectorized_txt = vectorizer(preprocessed_txt)
+    logging.info(f'Vectorized Text : {vectorized_txt}')
+
+    prediction = get_prediction(vectorized_txt)
+    logging.info(f'Prediction : {prediction}')
+
+    if prediction == 'negative':
         global negative
-        negative+=1
-
-    reveiws.insert(0, text)
+        negative += 1
+    else:
+        global positive
+        positive += 1
+    
+    reviews.insert(0, text)
     return redirect(request.url)
-
-    return render_template('index.html', data=data)
 
 if __name__ == "__main__":
     app.run()
