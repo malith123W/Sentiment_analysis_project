4d7d802dd26837ea56498bf6c90493e9799d0c00
malith123W <malithinduranga2001@gmail.com>
2025-05-10 23:11:34 +0530

    web_aplication_2

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect
+from helper import preprocessing, vectorizer, get_prediction
 
 app = Flask(__name__)
 
 data =dict()
-reveiws =['good product', 'bad product','i like it']
-positive=2
-negative=1
+reveiws =[]
+positive=0
+negative=0
 
 
 @app.route("/")
@@ -13,6 +14,26 @@ def index():
     data['reviews'] = reveiws
     data['positive'] = positive
     data['negative'] = negative
+    return render_template('index.html', data=data)
+
+
+@app.route("/", methods ={'post'})
+def my_post():
+    text = request.form['text']
+    preproced_text = preprocessing(text)
+    vectorized_text = vectorizer(preproced_text)
+    prediction = get_prediction(vectorized_text)
+
+    if prediction == 'positive':
+        global positive
+        positive+=1
+    else:
+        global negative
+        negative+=1
+
+    reveiws.insert(0, text)
+    return redirect(request.url)
+
     return render_template('index.html', data=data)
 
 if __name__ == "__main__":
